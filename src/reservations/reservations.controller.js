c61399635bd001c9e76f699377f4a8046711e331
Justin Allred <justinallred5@gmail.com>
2021-08-26 09:58:00 -0500

    methodNotAllowed middleware added; pushing for deployment testing of listing functionality

--- src/reservations/reservations.controller.js
+++ src/reservations/reservations.controller.js
@@ -3,13 +3,22 @@
  */
 const service = require("./reservations.service");
 
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+async function list(req, res, next) {
+  const { date } = req.query;
+  try {
+    const datesReservations = await service.list(date);
+    return res.json({ data: datesReservations });
+  } catch (err) {
+    next(err);
+  }
 }
 
-async function create(req, res) {}
+async function create(req, res, next) {
+  try {
+  } catch (err) {
+    next(err);
+  }
+}
 
 module.exports = {
   list: [list],
