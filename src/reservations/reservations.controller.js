8d0fb62a026056bcadb1836d539f272049418790
Justin Allred <justinallred5@gmail.com>
2021-08-30 11:39:52 -0500

    minor refactoring in order to pass tests

--- src/reservations/reservations.controller.js
+++ src/reservations/reservations.controller.js
@@ -9,21 +9,6 @@ const validProperties = [
   "people",
 ];
 
-function hasProperties(req, res, next) {
-  const { data = {} } = req.body;
-
-  try {
-    validProperties.forEach((property) => {
-      if (!data[property]) {
-        throw { status: 400, message: `A '${property}' property is required.` };
-      }
-    });
-    next();
-  } catch (err) {
-    next(err);
-  }
-}
-
 function hasValidProperties(req, res, next) {
   const { data = {} } = req.body;
   const dateFormat = /\d\d\d\d-\d\d-\d\d/;
@@ -31,69 +16,54 @@ function hasValidProperties(req, res, next) {
   const invalidFields = Object.keys(data).filter(
     (field) => !validProperties.includes(field)
   );
-
-  try {
-    if (invalidFields.length) {
-      throw {
-        status: 400,
-        message: `Invalid field(s): ${invalidFields.join(", ")}`,
-      };
-    }
-    if (typeof data.people !== "number" || data.people < 1) {
-      throw {
-        status: 400,
-        message: "the people field must be a number",
-      };
-    }
-    if (!data.reservation_date.match(dateFormat)) {
-      throw {
-        status: 400,
-        message: "the reservation_date field must be a valid date",
-      };
-    }
-    if (!data.reservation_time.match(timeFormat)) {
-      throw {
-        status: 400,
-        message: "the reservation_time field must be a valid time",
-      };
-    }
-    next();
-  } catch (err) {
-    next(err);
-  }
-}
-
-function isDuringStoreHours(req, res, next) {
   const { reservation_date, reservation_time } = req.body.data;
   const invalidDate = 2;
   const submitDate = new Date(reservation_date + " " + reservation_time);
   const dayAsNum = submitDate.getDay();
   const today = new Date();
+  let errors = [];
 
   try {
+    validProperties.forEach((property) => {
+      if (!data[property]) {
+        errors.push(`A '${property}' property is required.`);
+      }
+    });
+    if (invalidFields.length) {
+      errors.push(`Invalid field(s): ${invalidFields.join(", ")}`);
+    }
+    if (typeof data.people !== "number" || data.people < 1) {
+      errors.push("the people field must be a number.");
+    }
+    if (!data.reservation_date.match(dateFormat)) {
+      errors.push("the reservation_date field must be a valid date.");
+    }
+    if (!data.reservation_time.match(timeFormat)) {
+      errors.push("the reservation_time field must be a valid time.");
+    }
     if (submitDate < today) {
-      throw {
-        status: 400,
-        message: `The date and time cannot be in the past. Today is ${today}.`,
-      };
+      errors.push(
+        `The date and time must be in the future. Today is ${today}.`
+      );
     }
     if (dayAsNum === invalidDate) {
-      throw {
-        status: 400,
-        message: `The restaurant is not open on Tuesdays. Please select a different day.`,
-      };
+      errors.push(
+        `The restaurant is closed on Tuesdays. Please select a different day.`
+      );
     }
     if (reservation_time < "10:29:59") {
-      throw {
-        status: 400,
-        message: "The restaurant does not open until 10:30 a.m.",
-      };
+      errors.push("The restaurant does not open until 10:30 a.m.");
     }
     if (reservation_time >= "21:30:00") {
-      throw {
-        status: 400,
-        message: `The restaurant closes at 22:30 (10:30 pm). Please schedule your reservation at least one hour before close.`,
-      };
+      errors.push(
+        `The restaurant closes at 22:30 (10:30 pm). Please schedule your reservation at least one hour before close.`
+      );
+    }
+    if (errors.length > 1) {
+      throw { status: 400, message: errors };
+    }
+    if (errors.length == 1) {
+      throw { status: 400, message: errors[0] };
     }
     next();
   } catch (err) {
@@ -121,5 +91,5 @@ async function create(req, res, next) {
 
 module.exports = {
   list: [list],
-  create: [hasProperties, hasValidProperties, isDuringStoreHours, create],
+  create: [hasValidProperties, create],
 };
